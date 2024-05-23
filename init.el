dc52faa1aa3b59315c5666fe0d6ceda9ca21c78c
Alexis Purslane <alexispurslane@pm.me>
2024-05-23 07:36:36 -0400

    integrate embark with which-key

--- init.el
+++ init.el
@@ -261,9 +261,47 @@ that text object minus the .inner and .outer qualifiers.")
 ;;;; Embark
 
     (use-package embark
-	:commands (embark-act embark-dwim)))
+	:commands (embark-act embark-dwim)
+	:config
+	(defun embark-which-key-indicator ()
+	    "An embark indicator that displays keymaps using which-key.
+The which-key help message will show the type and value of the
+current target followed by an ellipsis if there are further
+targets."
+	    (lambda (&optional keymap targets prefix)
+		(if (null keymap)
+			(which-key--hide-popup-ignore-command)
+		    (which-key--show-keymap
+		     (if (eq (plist-get (car targets) :type) 'embark-become)
+			     "Become"
+			 (format "Act on %s '%s'%s"
+				 (plist-get (car targets) :type)
+				 (embark--truncate-target (plist-get (car targets) :target))
+				 (if (cdr targets) "…" "")))
+		     (if prefix
+			     (pcase (lookup-key keymap prefix 'accept-default)
+				 ((and (pred keymapp) km) km)
+				 (_ (key-binding prefix 'accept-default)))
+			 keymap)
+		     nil nil t (lambda (binding)
+				   (not (string-suffix-p "-argument" (cdr binding))))))))
 
-;;; Evil Mode plugins and keybindings to give Emacs a superior text editor
+	(setq embark-indicators
+	      '(embark-which-key-indicator
+		embark-highlight-indicator
+		embark-isearch-highlight-indicator))
+
+	(defun embark-hide-which-key-indicator (fn &rest args)
+	    "Hide the which-key indicator immediately when using the completing-read prompter."
+	    (which-key--hide-popup-ignore-command)
+	    (let ((embark-indicators
+		   (remq #'embark-which-key-indicator embark-indicators)))
+		(apply fn args)))
+
+	(advice-add #'embark-completing-read-prompter
+		    :around #'embark-hide-which-key-indicator)))
+
+;;; Evil Mode layer to give Emacs a superior text editor
 (defun core/editor-layer ()
     "'Emacs is a great OS, if only it had a good editor.' With
   the powerful text-object based command language of Vim, and the
